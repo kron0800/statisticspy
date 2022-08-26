7c4b6cea209efaeb7e47ac3e09b581805c6e80db
kron <smoya557@gmail.com>
2022-08-25 23:45:10 -0300

    stashed changes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 
 
 def menu():
-    os.system('cls')
     variables = input("""
 Bienvenido a Statistics in Python (beta)
 Inserte los datos a continuación:
@@ -21,19 +20,23 @@ Inserte los datos a continuación:
             temp = []
             continue
     data.sort()  
-
-    print(f'Original data > {data}')
+    
+    os.system('cls')
+    print('Data in order: [' + '  '.join(data) + ']')
     xi = set(data)
     xi = list(xi)
     xi.sort()
     
     fi = absolute_freq(data)
+    fr = relative_freq(fi, data)
 
-    print('Xi | fi |')
-    print('---|----|')
+    print('\nXi | fi |  fr   |')
+    print('---|----|-------|')
     for i in xi:
-        print(i, '|', fi.get(str(i)))
-    
+        print( str(i), '|', fi.get(str(i)), ' |' , fr.get(i), '|')
+    print('---|----|-------|')
+    total_fr = int(sum(fr.values()) * (10**3))/(10**3)
+    print(f'   | {sum(fi.values())} | {float(total_fr)} |')
 
 
 def absolute_freq(data):
@@ -45,11 +48,22 @@ def absolute_freq(data):
             temp = int(fi.get(i)) + 1
             temp = {i : temp}
             fi.update(temp)
+    fi.pop('')
     return fi
 
-            
 
-    print(xf)
+def relative_freq(fi, data):
+    fr= {'':''}
+    for key, value in fi.items():
+        division = int(value) / len(data)
+        division = int(division * (10**3))/(10**3)
+       # fr.append(str(division))
+        fr.setdefault(key, float(division))
+    fr.pop('')
+    return fr
+
+
+#print(xf)
 
 if __name__ == '__main__':
     menu()
